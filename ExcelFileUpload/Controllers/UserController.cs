73dafecc393c841f52f7d6080de58e709dbc754b
qaansari80 <qa.ansari80@gmail.com>
2022-10-22 05:20:28 +0500

    User Model Added with DbContext without injection

---
+++ ExcelFileUpload/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelFileUpload.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
