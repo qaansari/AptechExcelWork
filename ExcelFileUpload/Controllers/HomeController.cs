def25cef3d08926fa279ba4365db0146bf22ad47
qaansari80 <qa.ansari80@gmail.com>
2022-10-29 02:14:13 +0500

    Updated Changes

--- ExcelFileUpload/Controllers/HomeController.cs
+++ ExcelFileUpload/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.OleDb;
 
 namespace ExcelFileUpload.Controllers
 {
@@ -6,22 +8,30 @@ namespace ExcelFileUpload.Controllers
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IConfiguration _configuration;
         private readonly int UserID;
-        public HomeController(IHttpContextAccessor httpContextAccessor,IWebHostEnvironment webHostEnvironment)
+        public HomeController(IHttpContextAccessor httpContextAccessor, IWebHostEnvironment webHostEnvironment, IConfiguration configuration)
         {
             _httpContextAccessor = httpContextAccessor;
-            _webHostEnvironment= webHostEnvironment;
-            UserID = Convert.ToInt32(_httpContextAccessor.HttpContext.User.FindFirst("UserID").Value);
+            _webHostEnvironment = webHostEnvironment;
+            _configuration = configuration;
+            UserID = int.Parse(_httpContextAccessor.HttpContext.User.FindFirst("UserID").Value);
+
         }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult RenderedExcel(DataTable dt)
+        {
+            return View("Index", dt);
         }
         [HttpPost]
         public IActionResult uploadFile(IFormFile excelFile)
         {
             if (excelFile != null)
             {
+                //Save the uploaded file in a folder
                 var rootPath = _webHostEnvironment.WebRootPath;
                 var fileName = Path.GetFileName(excelFile.FileName);
                 var uploadPath = Path.Combine(rootPath, "ExcelFiles");
@@ -30,18 +40,33 @@ namespace ExcelFileUpload.Controllers
                     Directory.CreateDirectory(uploadPath);
                 }
                 var folderName = DateTime.UtcNow.AddHours(5).ToString("ddMMyyyyHHmmss");
-                var folderPath=Path.Combine(uploadPath, folderName);
+                var folderPath = Path.Combine(uploadPath, folderName);
                 if (!Directory.Exists(folderPath))
                 {
                     Directory.CreateDirectory(folderPath);
                 }
                 var filePath = Path.Combine(folderPath, fileName);
-                using (var stream = new FileStream(filePath,FileMode.Create))
+                using (var stream = new FileStream(filePath, FileMode.Create))
                 {
                     excelFile.CopyTo(stream);
                 }
+                //Read the Connection String for the excel file
             }
-            return RedirectToAction("Index");
+            var dt = readData("");
+            return RenderedExcel(dt);
+            //return RedirectToAction("Index");
+        }
+
+        public DataTable readData(string path)
+        {
+            path = "C:\\Users\\User\\source\\repos\\AptechExcelWork\\ExcelFileUpload\\wwwroot\\ExcelFiles\\29102022000529\\AptStdRecord.xlsx";
+            string excelCS = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=Yes'", path);
+            OleDbConnection con = new OleDbConnection(excelCS);
+            OleDbDataAdapter da = new OleDbDataAdapter("SELECT * from [Sheet1$]", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            
+            return dt;
         }
     }
 }
