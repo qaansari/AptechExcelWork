a4cf26670ed06c395780fba82e6fc9a30e97867c
qaansari80 <qa.ansari80@gmail.com>
2022-10-31 16:58:35 +0500

    Excel File Work Done

--- ExcelFileUpload/wwwroot/assets/js/custom.js
+++ ExcelFileUpload/wwwroot/assets/js/custom.js
@@ -1,3 +1,25 @@
-﻿$(document).ready(function () {
-    $('#excel_table').DataTable();
+﻿var table = $('#excel_table').DataTable();
+
+table.columns().flatten().each(function (colIdx) {
+    // Create the select list and search operation
+    var select = $('<select />')
+        .appendTo(
+            table.column(colIdx).header()
+        )
+        .on('change', function () {
+            table
+                .column(colIdx)
+                .search($(this).val())
+                .draw();
+        });
+
+    // Get the search data for the first column and add to the select list
+    table
+        .column(colIdx)
+        .cache('search')
+        .sort()
+        .unique()
+        .each(function (d) {
+            select.append($('<option value="' + d + '">' + d + '</option>'));
+        });
 });
